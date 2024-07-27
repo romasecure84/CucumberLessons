a56b1cb3c7b2138458edbbb65c68ab5e77789d91
ROMA2 <romasecure84@yandex.ru>
2024-07-28 01:50:46 +0400

    _01_LoginSteps class created

--- src/test/java/StepDefinitions/_01_LoginSteps.java
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -1,20 +1,34 @@
 package StepDefinitions;
 
+import Pages.DialogContent;
+import Utilities.BaseDriver;
 import io.cucumber.java.en.*;
+import org.testng.Assert;
+
 
 public class _01_LoginSteps {
+
+    DialogContent dialogContent = new DialogContent();
     @Given("Navigate to Opencart")
     public void navigateToOpencart() {
-        System.out.println("Opened Opencart");
+        BaseDriver.getDriver().get("https://opencart.abstracta.us/index.php?route=account/login");
+        BaseDriver.getDriver().manage().window().maximize();
     }
 
     @When("Enter the username and password and click login button")
     public void enterTheUsernameAndPasswordAndClickLoginButton() {
-        System.out.println("Login");
+
+        dialogContent.email.sendKeys("romasecure55555@gmail.com");
+
+        dialogContent.password.sendKeys("123456");
+
+        dialogContent.loginButton.click();
     }
 
     @Then("User should login successfully")
-    public void userShouldLoginSuccessfully() {
-        System.out.println("Login successfully");
+    public void userShouldLoginSuccessfully() throws InterruptedException {
+        Thread.sleep(2000);
+        Assert.assertTrue(dialogContent.myAccount.isDisplayed());
+        BaseDriver.quitDriver();
     }
 }
