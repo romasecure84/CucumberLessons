c63907c646389fe31004801c3c62c9f42a782b06
ROMA2 <romasecure84@yandex.ru>
2024-07-28 01:34:45 +0400

    BaseDriver class adapted

--- src/test/java/Utilities/BaseDriver.java
+++ src/test/java/Utilities/BaseDriver.java
@@ -1,33 +1,40 @@
 package Utilities;
 
-import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.time.Duration;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class BaseDriver {
-    public static WebDriver driver;
+//    private BaseDriver(){};
+    private static WebDriver driver;
 
     public static WebDriver getDriver(){
         Logger logger = Logger.getLogger("");
         logger.setLevel(Level.SEVERE);
         System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY,"true");
 
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-
-        Duration dr = Duration.ofSeconds(20);
-        driver.manage().timeouts().pageLoadTimeout(dr);
-        driver.manage().timeouts().implicitlyWait(dr);
-
+        if (driver == null) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--remote-allow-origins=*");
+            driver = new ChromeDriver(options);
+            driver.manage().window().maximize();
+        }
 
         return driver;
+    }
+    public static void quitDriver(){
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        if (driver != null){
+            driver.quit();
+            driver = null;
+        }
     }
 }
