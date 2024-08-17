e1a9e1a237687c0868bae6cb1aed4aba23e4b557
ROMA2 <romasecure84@yandex.ru>
2024-08-17 22:00:10 +0400

    for style changed

--- src/test/java/StepDefinitions/_08_AddDeleteAddressSteps.java
+++ src/test/java/StepDefinitions/_08_AddDeleteAddressSteps.java
@@ -19,9 +19,9 @@ public class _08_AddDeleteAddressSteps extends Parent {
     public void enterUsernameAndPasswordWithDataTable(DataTable dt) {
         List<List<String>> loginInfo = dt.asLists(String.class);
 
-        for (int i = 0; i < loginInfo.size(); i++){
-            WebElement element = dc.getWebElement(loginInfo.get(i).get(0));
-            sendKeysFunction(element,loginInfo.get(i).get(1));
+        for (List<String> strings : loginInfo) {
+            WebElement element = dc.getWebElement(strings.get(0));
+            sendKeysFunction(element, strings.get(1));
         }
     }
 
@@ -29,9 +29,9 @@ public class _08_AddDeleteAddressSteps extends Parent {
     public void enterTextToWebElementInAddressLine(DataTable dt) {
         List<List<String>> addressInfo = dt.asLists(String.class);
 
-        for (int i = 0; i < addressInfo.size(); i++){
-            WebElement element = fc.getWebElement(addressInfo.get(i).get(0));
-            sendKeysFunction(element,addressInfo.get(i).get(1));
+        for (List<String> strings : addressInfo) {
+            WebElement element = fc.getWebElement(strings.get(0));
+            sendKeysFunction(element, strings.get(1));
         }
     }
 
@@ -39,12 +39,12 @@ public class _08_AddDeleteAddressSteps extends Parent {
     public void selectCountryAndZone(DataTable dt) {
          List<List<String>> zoneInfo = dt.asLists(String.class);
 
-         for (int i = 0; i <  zoneInfo.size(); i++){
-             WebElement element = fc.getWebElement(zoneInfo.get(i).get(0));
-             Select select = new Select(element);
-             select.selectByVisibleText(zoneInfo.get(i).get(1));
+        for (List<String> strings : zoneInfo) {
+            WebElement element = fc.getWebElement(strings.get(0));
+            Select select = new Select(element);
+            select.selectByVisibleText(strings.get(1));
 
-         }
+        }
     }
 
     @Then("Should be deleted new address")
@@ -55,8 +55,8 @@ public class _08_AddDeleteAddressSteps extends Parent {
     @And("Click to DialogContent element with Data Table")
     public void clickToDialogContentElementWithDataTable(DataTable buttons) {
         List<String> buttonList = buttons.asList(String.class);
-        for (int i = 0; i < buttonList.size(); i++){
-            WebElement element = dc.getWebElement(buttonList.get(i));
+        for (String s : buttonList) {
+            WebElement element = dc.getWebElement(s);
             clickFunction(element);
         }
     }
